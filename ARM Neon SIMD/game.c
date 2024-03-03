dc722db445ccd078b9b4c1a8eccd7612015ca4e1
Myrzabekov Azamat <myrzabekoffazamat@gmail.com>
2024-03-04 07:59:59 +0900

    ARM Neon SIMD: tests passed

--- ARM Neon SIMD/game.c
+++ ARM Neon SIMD/game.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <math.h>
 #include <arm_neon.h>
+
+#include <string.h>
 
 void clearSides(int8_t* board, unsigned int M, unsigned int N){
     for(int i = 0; i < N + 2; ++i){
@@ -14,22 +17,11 @@ void clearSides(int8_t* board, unsigned int M, unsigned int N){
     }
 }
 
-void moveNegatedBoard(int8_t* t_board, int8_t* board, unsigned int M, unsigned int N){
-    for(int i = 0; i < M; ++i){
-        for(int j = 0; j < N; j += 8){ // use int8x8_t because {N mod 8 = -1}
-            int8x8_t v;
-            v = vld1_s8(board + (i + 1)*(N + 2) + j);
-            vneg_s8(v);
-            vst1_s8(t_board + (i + 1)*(N + 2) + j, v);
-        }
-    }
-}
-
 void countNeighbors(int8_t* t_board, int8_t* board, unsigned int M, unsigned int N){
     for(int i = 0; i < M; ++i){
-        for(int j = 0; j < N; j += 8){ // use int8x8_t because {N mod 8 = -1}
+        for(int j = 0; j < N; j += 8){
             int8x8_t v, t_v;
-            t_v = vld1_s8(t_board + (i + 1)*(N + 2) + j + 1);
+            t_v = vcreate_s8(0x00000000);
 
             // L, R
             v = vld1_s8(board + (i + 1)*(N + 2) + (j + 1) - 1); 
@@ -43,25 +35,25 @@ void countNeighbors(int8_t* t_board, int8_t* board, unsigned int M, unsigned int
             v = vld1_s8(board + (i + 1 + 1)*(N + 2) + (j + 1)); 
             t_v = vadd_s8(t_v, v);
 
-            // UR, UL
+            // // UR, UL
             v = vld1_s8(board + (i + 1 - 1)*(N + 2) + (j + 1) - 1); 
             t_v = vadd_s8(t_v, v);
             v = vld1_s8(board + (i + 1 - 1)*(N + 2) + (j + 1) + 1); 
             t_v = vadd_s8(t_v, v);
 
-            // DR, DL
+            // // DR, DL
             v = vld1_s8(board + (i + 1 + 1)*(N + 2) + (j + 1) - 1); 
             t_v = vadd_s8(t_v, v);
             v = vld1_s8(board + (i + 1 + 1)*(N + 2) + (j + 1) + 1); 
             t_v = vadd_s8(t_v, v);
 
             // store the vector back to memory
-            vst1_s8(t_board + (i + 1)*(N + 2) + j + 1, t_v);
+            vst1_s8(t_board + (i + 1)*(N + 2) + (j + 1), t_v);
         }
     }
 }
 
-void computeNextState(int8_t* t_board, int8_t* board, unsigned int M, unsigned int N){
+void computeNextState(int8_t* board, int8_t* t_board, unsigned int M, unsigned int N){
     for(int i = 0; i < M; ++i){
         for(int j = 0; j < N; ++j){
             if(t_board[(i + 1)*(N + 2) + (j + 1)] != 2)
@@ -73,35 +65,46 @@ void computeNextState(int8_t* t_board, int8_t* board, unsigned int M, unsigned i
 int main(){
     unsigned int M, N, G;
     scanf("%u %u %u ", &M, &N, &G);
+    G = N;
+    N = M;
 
     // Make N a multiple of 8 to ease use of Neon intrinsics
-    // unsigned int dN = max(ceil((double)N / 8) * 8 - N, 1) + 1;
-    // Assume for now that (N mod 8 = -1) 
+    // Assume for now that (N mod 8 = 0) for now
 
-    int8_t *board, *temp;
+    int8_t *board, *t_board;
     board = (int8_t*) malloc(sizeof(int8_t) * (M + 2) * (N + 2)); // extra padding for easier counting 
     t_board = (int8_t*) malloc(sizeof(int8_t) * (M + 2) * (N + 2));
+    memset((void *)board, 0, sizeof(int8_t) * (M + 2) * (N + 2));
+    memset((void *)t_board, 0, sizeof(int8_t) * (M + 2) * (N + 2));
 
     char t;
     for(int i = 0; i < M; ++i){
         for(int j = 0; j < N; ++j){
             scanf("%c", &t);
-            board[i * N + j] = (t == #);
+            board[(i + 1) * (N + 2) + (j + 1)] = (t == '#');
         }
         scanf("%c", &t);
     }
 
     for(int i = 0; i < G; ++i){
         clearSides(t_board, M, N);
-        moveNegatedBoard(t_board, board, M, N);
         countNeighbors(t_board, board, M, N);
         computeNextState(board, t_board, M, N);
     }
 
+    int8_t A[] = {1};
+    int8x8_t v = vld1_s8(A); 
+    v = vadd_s8(v, v);
+    vst1_s8(A, v);
+    printf("%d\n", A[0]);
+
     for(int i = 0; i < M; ++i){
         for(int j = 0; j < N; ++j){
-            printf("%c", (board[i * N + j] ? '#' : '.'));
+            printf("%c", (board[(i + 1) * (N + 2) + (j + 1)] ? '#' : '.'));
         }
         printf("\n");
     }
+
+    free(t_board);
+    free(board);
 }
